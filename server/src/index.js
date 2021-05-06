c679fc0b339f73582416bce2a81d3066edd68cac
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-05-06 15:43:34 +0530

    Completed the odyssey part 2.

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,37 @@
-const { ApolloServer, MockList } = require('apollo-server');
-const typeDefs = require('./schema');
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./schema.js";
+import resolvers from "./resolvers.js";
+import TrackAPI from "./datasource/track-api.js";
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => new MockList([6, 9]),
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+// const mocks = {
+//   Query: () => ({
+//     tracksForHome: () => new MockList([6, 9]),
+//   }),
+//   Track: () => ({
+//     id: () => "track_01",
+//     title: () => "Astro Kitty, Space Explorer",
+//     author: () => {
+//       return {
+//         name: "Grumpy Cat",
+//         photo:
+//           "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+//       };
+//     },
+//     thumbnail: () =>
+//       "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+//     length: () => 1210,
+//     modulesCount: () => 6,
+//   }),
+// };
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
 });
 
 server.listen().then(() => {
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+import { gql } from "apollo-server";
 
 const typeDefs = gql`
   type Query {
@@ -31,4 +31,4 @@ const typeDefs = gql`
   }
 `;
 
-module.exports = typeDefs;
+export default typeDefs;
